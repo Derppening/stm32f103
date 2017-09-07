e7acb014274738d4afa726769ce3106efc97ef3b
David Mak <david.18.19.21@gmail.com>
2017-09-07 20:52:16 +0800

    Documentation Pass 1
    Filling in the documentation for GPIO for now. The rest could wait...

--- inc/core/gpio.h
+++ inc/core/gpio.h
@@ -1,26 +1,74 @@
+// gpio.h
+//
+//
+
 #ifndef STM32F103_CORE_GPIO_H_
 #define STM32F103_CORE_GPIO_H_
 
-#include "stm32f10x_gpio.h"
-#include "stm32f10x_rcc.h"
+#include <stm32f10x_gpio.h>
+#include <stm32f10x_rcc.h>
 
 #include "util.h"
 
 class Gpio {
  public:
+  /**
+   * @brief Constructor accepting a port and a pin.
+   *
+   * @param port Port. Usually in the format of GPIOx, where x is an upper case character.
+   * @param pin Pin number. Usually in the format of GPIO_Pin_x, where x is a positive integer.
+   */
   Gpio(GPIO_TypeDef* port, uint16_t pin);
 
+  /**
+   * @brief Constructor accept a Pin object.
+   *
+   * @note For most use cases, this constructor is unnecessary. @c Gpio(GPIO_TypeDef*,uint16_t) is similar to this
+   * in every single way, except it lacks the overhead of delegating to the above constructor.
+   *
+   * @param pin Pin object.
+   */
   explicit Gpio(Pin pin) : Gpio(pin.first, pin.second) {}
 
+  /**
+   * @brief Performs initialization of this GPIO.
+   *
+   * @param Mode
+   * @param Speed
+   * @param rcc
+   */
   void Init(GPIOMode_TypeDef Mode, GPIOSpeed_TypeDef Speed, uint32_t rcc = 0);
   void Rcc(FunctionalState state, uint32_t rcc = 0);
 
-  void Set();
-  void Set(bool state);
+  /**
+   * @brief Set this GPIO to low position.
+   *
+   * @note Use @c Set(bool) instead. Please.
+   */
   void Reset();
+  /**
+   * @brief Set this GPIO to high position.
+   *
+   * @note Use @c Set(bool) instead. Please.
+   */
+  void Set();
+  /**
+   * @brief Sets this GPIO to @c state.
+   *
+   * @param state New state of GPIO
+   */
+  void Set(bool state);
+  /**
+   * @brief Swap the state of the GPIO.
+   */
   void Toggle();
 
+  /**
+   * @brief Read the current state of GPIO.
+   * @return Current state of GPIO
+   */
   uint8_t Read();
+
   uint16_t GetPinSource();
   GPIO_TypeDef* GetPort();
   uint16_t GetPin();
