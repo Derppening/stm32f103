6ad9ebad01b2a925f7ab53c4c38bfbd7e6bba040
Derppening <david.18.19.21@gmail.com>
2017-09-18 12:39:42 +0800

    Update CMake file for GCC flags
    Rewrite the structure of the file such that both C and C++ files will have dedicated warnings.
    
    Note that while compiling for target SPL_LIB, warnings will appear. These are warnings related to the STM32 SPL, and can safely be ignored.

--- cmake/compile_flags.cmake
+++ cmake/compile_flags.cmake
@@ -1,11 +1,17 @@
 set(CMAKE_C_STANDARD_REQUIRED YES)
 set(CMAKE_CXX_STANDARD_REQUIRED YES)
 
-set(CXX_WARN_FLAGS "-pedantic -Wall -Wextra\
-    -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Winvalid-pch\
-    -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wmissing-format-attribute -Wnoexcept -Wno-unused -Wodr\
-    -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo\
-    -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wswitch-enum -Wundef -Wuseless-cast\
-    -Wzero-as-null-pointer-constant")
+set(COMMON_WARN_FLAGS "-pedantic -Wall -Wextra\
+    -Wcast-align -Wcast-qual -Wdisabled-optimization -Wdouble-promotion -Wduplicated-cond -Wfloat-equal -Wformat=2\
+    -Winit-self -Winline -Winvalid-pch -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs\
+    -Wmissing-format-attribute -Wno-unused -Wnull-dereference -Wodr -Wpointer-arith -Wredundant-decls -Wshadow\
+    -Wsign-conversion -Wstrict-overflow=5 -Wswitch-default -Wswitch-enum -Wwrite-strings -Wundef -Wuninitialized\
+    -Wunreachable-code")
 
+set(C_WARN_FLAGS "${COMMON_WARN_FLAGS} -Wbad-function-cast -Wnested-externs -Wmissing-prototypes -Wold-style-definition\
+    -Wstrict-prototypes")
+set(CXX_WARN_FLAGS "${COMMON_WARN_FLAGS} -Wctor-dtor-privacy -Wnoexcept -Wold-style-cast -Woverloaded-virtual\
+    -Wsign-promo -Wstrict-null-sentinel -Wuseless-cast -Wzero-as-null-pointer-constant")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${C_WARN_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXX_WARN_FLAGS}")
